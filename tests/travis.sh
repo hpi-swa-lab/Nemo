efcf2918ce8ed9b07e8a67034906736741a75d76
Leo Selig <selig.leo@gmail.com>
2015-01-27 12:29:22 +0100

    Deploy failing images
    Changed travis script to deploy image to lively kernel with the suffix "_failing" for debugging purposes

--- tests/travis.sh
+++ tests/travis.sh
@@ -20,4 +20,23 @@ if [ $exitcode -eq 0 ]; then
 	curl -T *.sources http://www.lively-kernel.org/babelsberg/BP2014H1/
     fi
 fi
+if [ $exitcode -eq 1 ]; then
+    if [ "$ST" == "Squeak-4.5" ]; then
+	set -x
+	echo Uploading $BUILDER_CI_HOME/images/*.image
+	# cp $BUILDER_CI_HOME/images/*.image BP2014H1_failing.image
+	cd $PROJECT_HOME
+	echo $PWD
+	cp $BUILDER_CI_HOME/images/*.image BP2014H1_failing.image
+	cp $BUILDER_CI_HOME/images/*.changes BP2014H1_failing.changes
+	cp $BUILDER_CI_HOME/sources/*.sources .
+	echo "$VM_PATH/Linux/squeak -nosound -plugins "$VM_PATH/Linux" -encoding Latin1 -nodisplay BP2014H1_failing.image"
+	bash -c "while true; do printf .; sleep 10; done" &
+	$VM_PATH/Linux/squeak -nosound -plugins "$VM_PATH/Linux" -encoding Latin1 -nodisplay BP2014H1_failing.image tests/build_image.st
+	echo "uploading..."
+	curl -T BP2014H1_failing.image http://www.lively-kernel.org/babelsberg/BP2014H1/
+	curl -T BP2014H1_failing.changes http://www.lively-kernel.org/babelsberg/BP2014H1/
+	curl -T *.sources http://www.lively-kernel.org/babelsberg/BP2014H1/
+    fi
+fi
 exit $exitcode
