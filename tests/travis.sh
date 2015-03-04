f8dd6e5242345e0616f8cf6b033e1cd517fd222a
Johanna Appel <j.appel.online@gmail.com>
2015-03-04 16:56:22 +0100

    remove trunk image push

--- tests/travis.sh
+++ tests/travis.sh
@@ -38,21 +38,21 @@ if [ $exitcode -eq 1 ]; then
 	curl -T BP2014H1_failing.changes http://www.lively-kernel.org/babelsberg/BP2014H1/
 	curl -T *.sources http://www.lively-kernel.org/babelsberg/BP2014H1/
     fi
-    if [ "$ST" == "Squeak-Trunk" ]; then
-	set -x
-	echo Uploading $BUILDER_CI_HOME/images/*.image
-	cd $PROJECT_HOME
-	echo $PWD
-	cp $BUILDER_CI_HOME/images/*.image BP2014H1_failing_trunk.image
-	cp $BUILDER_CI_HOME/images/*.changes BP2014H1_failing_trunk.changes
-	cp $BUILDER_CI_HOME/sources/*.sources .
-	echo "$VM_PATH/Linux/squeak -nosound -plugins "$VM_PATH/Linux" -encoding Latin1 -nodisplay BP2014H1_failing_trunk.image"
-	bash -c "while true; do printf .; sleep 10; done" &
-	$VM_PATH/Linux/squeak -nosound -plugins "$VM_PATH/Linux" -encoding Latin1 -nodisplay BP2014H1_failing_trunk.image tests/build_image.st
-	echo "uploading..."
-	curl -T BP2014H1_failing_trunk.image http://www.lively-kernel.org/babelsberg/BP2014H1/
-	curl -T BP2014H1_failing_trunk.changes http://www.lively-kernel.org/babelsberg/BP2014H1/
-	curl -T *.sources http://www.lively-kernel.org/babelsberg/BP2014H1/
-    fi
+    # if [ "$ST" == "Squeak-Trunk" ]; then
+	# set -x
+	# echo Uploading $BUILDER_CI_HOME/images/*.image
+	# cd $PROJECT_HOME
+	# echo $PWD
+	# cp $BUILDER_CI_HOME/images/*.image BP2014H1_failing_trunk.image
+	# cp $BUILDER_CI_HOME/images/*.changes BP2014H1_failing_trunk.changes
+	# cp $BUILDER_CI_HOME/sources/*.sources .
+	# echo "$VM_PATH/Linux/squeak -nosound -plugins "$VM_PATH/Linux" -encoding Latin1 -nodisplay BP2014H1_failing_trunk.image"
+	# bash -c "while true; do printf .; sleep 10; done" &
+	# $VM_PATH/Linux/squeak -nosound -plugins "$VM_PATH/Linux" -encoding Latin1 -nodisplay BP2014H1_failing_trunk.image tests/build_image.st
+	# echo "uploading..."
+	# curl -T BP2014H1_failing_trunk.image http://www.lively-kernel.org/babelsberg/BP2014H1/
+	# curl -T BP2014H1_failing_trunk.changes http://www.lively-kernel.org/babelsberg/BP2014H1/
+	# curl -T *.sources http://www.lively-kernel.org/babelsberg/BP2014H1/
+    # fi
 fi
 exit $exitcode
